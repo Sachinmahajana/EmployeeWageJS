cad14257c0f77c85c6d3a6ccbdd2a035ae7108a2
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-14 14:07:07 +0530

    [Sachin] Adding Adding UC1-EmpAttendance

---
+++ EmployeeWage.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Employeewage program");
+// UC1....Emp Attendance Present or Absent...
+const IS_PRESENT = 1;
+let empInput = Math.floor(Math.random()*10)%2;
+if(empInput == IS_PRESENT)
+{
+    console.log("Employee is Present");
+}
+else
+{
+    console.log("Employee is Absent");
+}
+
+
+
---
+++ Sample.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 12;
+console.log(a+b);
+
+let age = 18;
+if(age>=18 && age<=100)
+{
+   console.log("Eligible for voting");
+}
+else 
+{
+    console.log("Not Eligible for voting");
+}
