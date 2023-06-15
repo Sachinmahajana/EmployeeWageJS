bb5307de1326ebbebd3311f8b9249ec761ce639f
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-15 10:15:27 +0530

    [Sachin] Adding UC2-CalculateDailyWage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,15 +1,28 @@
 console.log("Welcome to Employeewage program");
 // UC1....Emp Attendance Present or Absent...
-const IS_PRESENT = 1;
-let empInput = Math.floor(Math.random()*10)%2;
-if(empInput == IS_PRESENT)
+const IsFulltime = 1;
+const IsParttime = 2;
+const EmpRatePerHr = 20;
+let empWage = 0;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+
+switch(empCheck)
 {
-    console.log("Employee is Present");
-}
-else
-{
-    console.log("Employee is Absent");
-}
+    case IsFulltime:
+        empHrs = 8;
+        break;
+    case IsParttime:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
+        break;
+} 
+//..UC2..Calculate Wage...
+empWage = EmpRatePerHr*empHrs;
+console.log("EmployeeWage is:" + empWage);                   
+
 
 
 
